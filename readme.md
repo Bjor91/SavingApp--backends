9ef5acba272ba04d23265a615fd5b8d002ee00b7
Bjorn Hagen <bjornhagen3@live.no>
2023-09-18 15:33:01 +0200

    changes readme and deleted dead code

--- readme.md
+++ readme.md
@@ -1,13 +1,30 @@
 ## Task description
-Here you can describe what tasks you decided to solve.
 
-Spring-boot app 
+Design and implement a Spring Boot application that efficiently extracts personal financial data, and subsequently stores the processed data into a remote PostgreSQL database.
+
+## Prerequisites
+
+Before you begin, ensure you have the following installed on your local machine:
+Java JDK: This project was developed with Java 17.0.7
+This is a Maven project, so you'll need it for dependency management. If you haven't installed Maven, you can follow the instructions here.
 
 ## How to run
+
 Navigate to the class SavingAppApplicationTests.java and run it.
 
 NB! Can take a few minutes send the data to the server.
 
+## Important Notes
+
+- The application processes Excel files and uploads their data to a remote PostgreSQL database. The excel file is an edited sample of my own expenses.
+- Currently, the front-end configuration expects the back-end services to run LOCALLY. As a result, to access and interact with the web application effectively, both the front-end and the back-end components should be executed on the same machine.
+
+Please ensure that you run the Spring Boot backend locally when using the web application to avoid connectivity issues.
 
 ## Comments
-Comments regaring design choices, decisions, or anything at all.
+
+Improvements and Reflections
+
+- Hosting Configuration: The current setup, where the back-end application runs locally while the database is hosted remotely, isn't the most efficient for wider public access. Ideally, both the back-end application and the database should be hosted remotely. This ensures that users can access the service seamlessly without needing to run any local instances.
+- File Processing: While the application currently processes Excel files, future iterations could support various file formats, enhancing the flexibility and user-friendliness of the service.
+- Security Considerations: Given that we're dealing with personal expenses, it's vital to consider strengthening the security measures. This includes encrypting sensitive data, implementing rigorous authentication and authorization processes, and regular security audits.
--- src/main/java/com/saveIt/SavingApp/transaction/TransactionGenerator.java
+++ src/main/java/com/saveIt/SavingApp/transaction/TransactionGenerator.java
@@ -3,52 +3,11 @@ package com.saveIt.SavingApp.transaction;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import org.apache.poi.ss.usermodel.*;
 import org.springframework.core.io.ClassPathResource;
 
 public class TransactionGenerator {
-    /*
-     * public static List<Transaction> generateTransactions() {
-     * List<Transaction> transactions = new ArrayList<>();
-     * 
-     * String[] accountIds = { "acc123", "acc789" };
-     * String[] descriptions = {
-     * "Grocery Store", "Online Shopping", "Lunch Out"
-     * };
-     * String[] currencies = { "NOK" };
-     * String[] dates = generateRandomAugustDates(100); // Generate 100 random dates
-     * in August
-     * 
-     * Random random = new Random();
-     * 
-     * int transactionIndex = 14;
-     * for (int i = 0; i < dates.length; i++) {
-     * for (String accountId : accountIds) {
-     * String txnId = "txn" + String.format("%03d", transactionIndex++);
-     * String date = dates[i];
-     * String description = descriptions[random.nextInt(descriptions.length)];
-     * double amount = (random.nextDouble() * 100) * -1; // Negative random amount
-     * between 0 and -100
-     * String currency = currencies[0];
-     * transactions.add(new Transaction(txnId, date, description, amount, currency,
-     * accountId));
-     * }
-     * }
-     * 
-     * return transactions;
-     * }
-     * 
-     * public static String[] generateRandomAugustDates(int count) {
-     * String[] dates = new String[count];
-     * Random random = new Random();
-     * for (int i = 0; i < count; i++) {
-     * int day = random.nextInt(31) + 1; // Random day between 1 and 31
-     * dates[i] = "2023-08-" + String.format("%02d", day);
-     * }
-     * return dates;
-     * }
-     */
+
     public static List<Transaction> generateTransactionsFromExcel() throws Exception {
         List<Transaction> transactions = new ArrayList<>();
 
@@ -58,10 +17,9 @@ public class TransactionGenerator {
         Workbook workbook = WorkbookFactory.create(is);
         Sheet sheet = workbook.getSheetAt(0);
 
-        for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) { // Skipping header row
+        for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
             Row currentRow = sheet.getRow(i);
 
-            // Check if row or cells are empty before processing
             if (currentRow == null)
                 continue;
 
@@ -81,10 +39,4 @@ public class TransactionGenerator {
         is.close();
         return transactions;
     }
-    /*
-     * private static String safeCellValue(Cell cell) {
-     * return (cell == null || cell.toString().trim().isEmpty()) ? "" :
-     * cell.toString().trim();
-     * }
-     */
 }
